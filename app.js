96f17d588f313922a7561610be620056478e027f
{Yi} <you@domain.com>
2019-09-04 07:05:08 +0000

    edit mongoose connect

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ var commentRoutes= require("./routes/comments"),
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 // mongoose.connect("mongodb://localhost/Yelp_Camp");
-mongoose.connect("mongodb+srv://Yi:123@cluster0-1xnxh.mongodb.net/test?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://Yi:123@cluster0-1xnxh.mongodb.net/test?retryWrites=true&w=majority",{
+    useNewUrlParser:true;
+    useCreateIndex:true;
+})
 app.use(express.static(__dirname+"/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
