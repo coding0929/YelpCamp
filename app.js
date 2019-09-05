dde48e8ae39207b7cc643eb70c0078c155faf115
{Yi} <you@domain.com>
2019-09-05 06:11:33 +0000

    app

--- app.js
+++ app.js
@@ -55,6 +55,6 @@ app.use(authRoutes);
 
 //seedDB();
 
-app.listen(process.env.PORT || 9000, function(){
+app.listen(process.env.PORT || 9000, process.env.IP, function(){
   console.log("YelpCamp has started!");
 });
